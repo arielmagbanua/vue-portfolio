26845c5b253c26ab6fb3bf193b323846244587f7
unknown <magbanua.c.ariel@gmail.com>
2022-08-17 19:12:19 +0800

    Improved the linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,18 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  "root": true,
-  "extends": [
+  root: true,
+  extends: [
     "plugin:vue/vue3-essential",
+    "plugin:vue/vue3-recommended",
     "eslint:recommended",
     "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier"
+    "@vue/eslint-config-prettier",
   ],
-  "parserOptions": {
-    "ecmaVersion": "latest"
-  }
-}
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    quotes: ['error', 'single'],
+  },
+};
