71b86c85f1e3227719a3967d8e6ce35281389688
unknown <magbanua.c.ariel@gmail.com>
2022-08-21 08:00:05 +0800

    Added education section.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -8,6 +8,20 @@ import ProfileIntroduction from '@/components/ProfileIntroduction.vue';
       <div class="grid grid-cols-1">
         <ProfileIntroduction />
         <!-- Education and Contact Info -->
+        <div class="grid grid-cols-1 py-8">
+          <div class="flex flex-auto py-4">
+            <font-awesome-icon icon="fa-solid fa-graduation-cap" class="text-gray-800 w-8 h-8 mr-2" />
+            <h2 class="text-gray-800 text-2xl font-bold">Education</h2>
+          </div>
+          <div class="grid grid-cols-1 ml-4">
+            <h3 class="text-gray-800 text-2xl font-semibold">Bachelor of Science (BS), Computer Engineering</h3>
+            <h4 class="text-gray-500 text-lg">Holy Cross of Davao College, Davao City, Philippines</h4>
+            <h5 class="text-gray-500 text-sm mb-2">2007 - 2012</h5>
+            <p class="text-gray-500 text-lg">
+              Studied electronics, computer networks, and software engineering.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
     <div class="skills">
